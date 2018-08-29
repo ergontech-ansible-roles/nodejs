e454f8b0136d550184853192ff6fd8bf83190b11
Colby Olson <me@colbyolson.com>
2018-08-29 14:46:13 -0700

    remove import line, its echoed in travis file now; fix spacing

--- spec/localhost/nodejs_spec.rb
+++ spec/localhost/nodejs_spec.rb
@@ -1,13 +1,11 @@
-#require 'spec_helper'
-
 describe file('/etc/apt/sources.list.d/deb_nodesource_com_node_8_x.list') do
   it { should be_file }
 end
 
 describe command('which npm') do
-    its(:exit_status) { should eq 0 }
+  its(:exit_status) { should eq 0 }
 end
 
 describe command('which yarn') do
-    its(:exit_status) { should eq 0 }
+  its(:exit_status) { should eq 0 }
 end
