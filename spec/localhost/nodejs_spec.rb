0bad8930b7e1d35585b6e7395f454eb180e48f6a
Colby Olson <me@colbyolson.com>
2018-08-28 19:33:38 -0700

    add some tests

--- spec/localhost/nodejs_spec.rb
+++ spec/localhost/nodejs_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
-describe file('/etc/passwd') do
+describe file('/etc/apt/sources.d/node_8*') do
   it { should be_file }
+end
+
+describe command('which npm') do
+    its(:exit_status) { should eq 0 }
+end
+
+describe command('which yarn') do
+    its(:exit_status) { should eq 0 }
 end
